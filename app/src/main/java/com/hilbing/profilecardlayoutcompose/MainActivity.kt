fee7bb3a16da070411f762ed7b3622605ce98a51
Gretel Hilbing <ghilbing@gmail.com>
2021-09-07 15:38:13 -0700

    Theme and customazing colors

--- app/src/main/java/com/hilbing/profilecardlayoutcompose/MainActivity.kt
+++ app/src/main/java/com/hilbing/profilecardlayoutcompose/MainActivity.kt
@@ -7,13 +7,12 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -25,7 +24,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreen()
+            ProfileCardLayoutComposeTheme {
+                MainScreen()
+            }
         }
     }
 }
@@ -84,11 +85,33 @@ fun ProfilePicture(){
 
 @Composable
 fun ProfileContent(){
-    Text(text = "Gretel")
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            ) {
+
+        Text(
+            text = "Gretel",
+            style = MaterialTheme.typography.h5
+        )
+
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Text(
+                text = "Active now",
+                style = MaterialTheme.typography.body2
+            )
+        }
+
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MainScreen()
+    ProfileCardLayoutComposeTheme {
+        MainScreen()
+    }
+
 }
